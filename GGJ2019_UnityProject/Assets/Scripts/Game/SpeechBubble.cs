ba8ac9e2f3fcadbc35204037fc89108f2c28a0f4
Eynard Thibault <contact@thibault-eynard.com>
2019-01-26 16:20:03 +0100

    Updated Speech Bubble

--- GGJ2019_UnityProject/Assets/Scripts/Game/SpeechBubble.cs
+++ GGJ2019_UnityProject/Assets/Scripts/Game/SpeechBubble.cs
@@ -13,14 +13,23 @@ public class SpeechBubble : MonoBehaviour
     public void ActivateBubble(PlanetCharacter target, LevelState state)
     {
         m_target = target.gameObject;
+        string newText = string.Empty;
         if (state == LevelState.Accusing)
         {
-            m_bubbleText.text = target.GetCharacterDescriptor().accusedSpeech;
+            newText = target.GetCharacterDescriptor().accusedSpeech;
         }
         else if (state == LevelState.Investigating)
         {
-            m_bubbleText.text = target.GetCharacterDescriptor().speech;
+            newText = target.GetCharacterDescriptor().speech;
         }
+
+        TextGenerator textGen = new TextGenerator();
+        TextGenerationSettings generationSettings = m_bubbleText.GetGenerationSettings(m_bubbleText.rectTransform.rect.size);
+        float textHeight = textGen.GetPreferredHeight(newText, generationSettings);
+        m_bubbleText.text = newText;
+        m_bubbleText.rectTransform.sizeDelta = new Vector2(m_bubbleText.rectTransform.sizeDelta.x, textHeight);
+        m_rect.sizeDelta = new Vector2(m_rect.sizeDelta.x, textHeight + Mathf.Abs(m_bubbleText.rectTransform.anchoredPosition.y) * 2f);
+
         transform.position = new Vector2(m_target.transform.position.x, m_target.transform.position.y + 2.5f);
         gameObject.SetActive(true);
     }
