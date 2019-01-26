1ae6e7c548b155d414bbb97206a3361c174b3113
Breizhpanda <ferellec.gabin.isart@gmail.com>
2019-01-26 23:06:16 +0100

    Ajout bg fading sur accusation popup

--- GGJ2019_UnityProject/Assets/Scripts/Gui/AccusationPopup.cs
+++ GGJ2019_UnityProject/Assets/Scripts/Gui/AccusationPopup.cs
@@ -11,9 +11,11 @@ public class AccusationPopup : MonoBehaviour
     [SerializeField] private Image m_inspectorPortraitImg;
     [SerializeField] private RectTransform m_titleRect;
     [SerializeField] private RectTransform m_portraitRect;
+    [SerializeField] private Image m_bgImage;
     [SerializeField] private float m_arrivalSpeed;
     [SerializeField] private float m_remainingTime;
     [SerializeField] private float m_portraitSlowDecalValue;
+    [SerializeField] private float m_bgFadeTime;
 
     void Awake()
     {
@@ -22,9 +24,10 @@ public class AccusationPopup : MonoBehaviour
 
     public Cx.Routine AccusationPopupAnim()
     {
+        m_bgImage.color = new Color(m_bgImage.color.r, m_bgImage.color.g, m_bgImage.color.b, 0f);
         Vector2 startTitlePos = new Vector2(Screen.currentResolution.width * 0.5f + m_titleRect.rect.width * 0.5f, m_titleRect.position.y);
         Vector2 startPortraitPos = new Vector2(-Screen.currentResolution.width * 0.5f - m_portraitRect.rect.width * 0.5f, m_portraitRect.position.y);
-        Vector2 portraitInPos = new Vector2(0f/*-Screen.currentResolution.width * 0.5f + m_portraitRect.rect.width*0.5f*/, m_portraitRect.position.y);
+        Vector2 portraitInPos = new Vector2(0f, m_portraitRect.position.y);
         m_titleRect.anchoredPosition = startTitlePos;
         m_portraitRect.anchoredPosition = startTitlePos;
         return Cx.Sequence(
@@ -34,13 +37,16 @@ public class AccusationPopup : MonoBehaviour
                }),
                Cx.Parallel(
                    Cx.ValueTo((float f) => m_titleRect.anchoredPosition = new Vector2(f, startTitlePos.y), startTitlePos.x, 0f, m_arrivalSpeed),
-                   Cx.ValueTo((float f) => m_portraitRect.anchoredPosition = new Vector2(f, startPortraitPos.y), startPortraitPos.x, portraitInPos.x, m_arrivalSpeed)
+                   Cx.ValueTo((float f) => m_portraitRect.anchoredPosition = new Vector2(f, startPortraitPos.y), startPortraitPos.x, portraitInPos.x, m_arrivalSpeed),
+                   Cx.ValueTo((float f) => m_bgImage.color = new Color(m_bgImage.color.r, m_bgImage.color.g, m_bgImage.color.b, f), 0f, 0.5f, m_bgFadeTime)
                    ),
                Cx.ValueTo((float f) => m_portraitRect.anchoredPosition = new Vector2(f, startPortraitPos.y), portraitInPos.x, portraitInPos.x + m_portraitSlowDecalValue, m_remainingTime),
                Cx.Parallel(
                   Cx.ValueTo((float f) => m_titleRect.anchoredPosition = new Vector2(f, startTitlePos.y), 0f, Screen.currentResolution.width * 0.5f + m_titleRect.rect.width * 0.5f, m_arrivalSpeed),
-                  Cx.ValueTo((float f) => m_portraitRect.anchoredPosition = new Vector2(f, startPortraitPos.y), portraitInPos.x + m_portraitSlowDecalValue, -Screen.currentResolution.width * 0.8f - m_portraitRect.rect.width * 0.5f, m_arrivalSpeed)
-                   ),
+                  Cx.ValueTo((float f) => m_portraitRect.anchoredPosition = new Vector2(f, startPortraitPos.y), portraitInPos.x + m_portraitSlowDecalValue, -Screen.currentResolution.width * 0.8f - m_portraitRect.rect.width * 0.5f, m_arrivalSpeed),
+                  Cx.ValueTo((float f) => m_bgImage.color = new Color(m_bgImage.color.r, m_bgImage.color.g, m_bgImage.color.b, f), 1f, 0f, m_bgFadeTime)
+
+                  ),
                Cx.Call(() =>
                {
                    gameObject.SetActive(false);
