3648dbec1a319361123094f0d5c98507f22ef911
Eynard Thibault <contact@thibault-eynard.com>
2019-01-26 23:22:20 +0100

    Updated bubbles

--- GGJ2019_UnityProject/Assets/Scripts/Game/SpeechBubble.cs
+++ GGJ2019_UnityProject/Assets/Scripts/Game/SpeechBubble.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class SpeechBubble : MonoBehaviour
 {
+    [SerializeField] private RectTransform m_characterNameGroup;
+    [SerializeField] private Text m_characterNameText;
     [SerializeField] private Text m_bubbleText;
     [SerializeField] private RectTransform m_rect;
     private GameObject m_target;
@@ -13,19 +15,28 @@ public class SpeechBubble : MonoBehaviour
     public void ActivateBubble(PlanetCharacter target, LevelState state)
     {
         m_target = target.gameObject;
+        PlanetCharacterDescriptor targetDescriptor = target.GetCharacterDescriptor();
         string newText = string.Empty;
         if (state == LevelState.Accusing)
         {
-            newText = target.GetCharacterDescriptor().accusedSpeech;
+            newText = targetDescriptor.accusedSpeech;
         }
         else if (state == LevelState.Investigating || state == LevelState.Starting)
         {
-            newText = target.GetCharacterDescriptor().speech;
+            newText = targetDescriptor.speech;
         }
+
         m_bubbleText.text = newText;
         float textHeight = m_bubbleText.preferredHeight;
         m_bubbleText.rectTransform.sizeDelta = new Vector2(m_bubbleText.rectTransform.sizeDelta.x, textHeight);
-        m_rect.sizeDelta = new Vector2(m_rect.sizeDelta.x, textHeight + Mathf.Abs(m_bubbleText.rectTransform.anchoredPosition.y) * 2f);
+        m_rect.sizeDelta = new Vector2(m_rect.sizeDelta.x, textHeight + Mathf.Abs(m_bubbleText.rectTransform.anchoredPosition.y) + m_bubbleText.rectTransform.offsetMin.x);
+
+        m_characterNameText.text = targetDescriptor.characterName;
+        float nameWidth = m_characterNameText.preferredWidth;
+        m_characterNameGroup.sizeDelta = new Vector2(
+            Mathf.Abs(m_characterNameText.rectTransform.offsetMin.x * 2) + nameWidth,
+            m_characterNameGroup.sizeDelta.y
+        );
 
         this.Update();
         gameObject.SetActive(true);
