837f3b6ab6822e7df832ab236f9520d7b574c2ab
Breizhpanda <ferellec.gabin.isart@gmail.com>
2019-01-26 21:42:21 +0100

    Animation mission title

--- GGJ2019_UnityProject/Assets/Scripts/Gui/MissionTitle.cs
+++ GGJ2019_UnityProject/Assets/Scripts/Gui/MissionTitle.cs
@@ -11,7 +11,7 @@ public class MissionTitle : MonoBehaviour
     [SerializeField] private Text m_planetNameText;
     [SerializeField] private RectTransform m_titleRect;
     [SerializeField] private RectTransform m_planetNameRect;
-    [SerializeField] private float m_animSpeed;
+    [SerializeField] private float m_arrivalSpeed;
     [SerializeField] private float m_remainingTime;
 
     void Awake()
@@ -20,19 +20,30 @@ public class MissionTitle : MonoBehaviour
     }
 
     public Cx.Routine TitleAnimation(int missionCount, string missionName, string planetName)
-    {        
-        /*m_titleRect.rect. = new Vector2(-Screen.width * 0.5f, m_titleRect.position.y);
-        m_planetNameRect.position = new Vector2(-Screen.width * 0.5f, m_planetNameRect.position.y);*/
-
+    {
+        Vector2 startTitlePos = new Vector2(-Screen.currentResolution.width * 0.5f - m_titleRect.rect.width*0.5f, m_titleRect.position.y);        
+        Vector2 startPlanetNamePos = new Vector2(Screen.currentResolution.width * 0.5f + m_planetNameRect.rect.width * 0.5f, m_planetNameRect.position.y);
+        m_titleRect.anchoredPosition = startTitlePos;
+        m_planetNameRect.anchoredPosition = startTitlePos;
         return Cx.Sequence(
                Cx.Call(() =>
                {
+                   
+                   
                    Debug.Log("dans routine de title animation");
                    m_titleText.text = string.Format("Mission {0} : {1}", missionCount, missionName);
                    m_planetNameText.text = string.Format("Destination : {0}", planetName);
                    gameObject.SetActive(true);
                }),
+               Cx.Parallel(
+                   Cx.ValueTo((float f) => m_titleRect.anchoredPosition = new Vector2(f, startTitlePos.y), startTitlePos.x, 0f, m_arrivalSpeed),
+                   Cx.ValueTo((float f) => m_planetNameRect.anchoredPosition = new Vector2(f, startPlanetNamePos.y), startPlanetNamePos.x, 0f, m_arrivalSpeed)
+                   ),
                Cx.Delay(m_remainingTime),
+               Cx.Parallel(
+                  Cx.ValueTo((float f) => m_titleRect.anchoredPosition = new Vector2(f, startTitlePos.y), 0f, Screen.currentResolution.width * 0.5f + m_titleRect.rect.width * 0.5f, m_arrivalSpeed),
+                  Cx.ValueTo((float f) => m_planetNameRect.anchoredPosition = new Vector2(f, startPlanetNamePos.y), 0f, -Screen.currentResolution.width * 0.5f - m_planetNameRect.rect.width * 0.5f, m_arrivalSpeed)
+                   ),
                Cx.Call(() =>
                {
                    gameObject.SetActive(false);
