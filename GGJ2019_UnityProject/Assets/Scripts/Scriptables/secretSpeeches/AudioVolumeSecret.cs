5cd9188972a8870e259293ae371d30533fc91571
Breizhpanda <ferellec.gabin.isart@gmail.com>
2019-01-27 04:08:52 +0100

    created options script and audioVolume secret descriptor

---
+++ GGJ2019_UnityProject/Assets/Scripts/Scriptables/secretSpeeches/AudioVolumeSecret.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolumeSecret : HiddenSpeechParams
+{
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float m_volumeValueToUnlock;
+    public int volumeValueToUnlock { get { return volumeValueToUnlock; } }
+}
